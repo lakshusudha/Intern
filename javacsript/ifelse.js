a5c798c31a1b337549310b99fe9d02dc88531119
lakshusudha <164891763+lakshusudha@users.noreply.github.com>
2025-10-31 20:17:56 +0530

    Create ifelse.js

---
+++ javacsript/ifelse.js
@@ -0,0 +1,82 @@
+
+// 1//
+let num = -5;
+if (num > 0) {
+    console.log("Positive");
+} else {
+    console.log("Negative");
+}
+
+// 2//
+let n = 8;
+if (n % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+// 3//
+let age = 16;
+if (age >= 18) {
+    console.log("Eligible to Vote");
+} else {
+    console.log("Not Eligible");
+}
+
+// 4//
+let value = "hello";
+console.log(typeof value);   
+
+// 5//
+let a = 10, b = 20;
+if (a > b) {
+    console.log(a + " is greater");
+} else {
+    console.log(b + " is greater");
+}
+
+// 6//
+let s1 = "Apple";
+let s2 = "apple";
+if (s1 === s2) {
+    console.log("Strings are equal");
+} else {
+    console.log("Strings are not equal");
+}
+
+// 7//
+let num5 = 25;
+if (num5 % 5 === 0) {
+    console.log("Multiple of 5");
+} else {
+    console.log("Not a multiple of 5");
+}
+
+// 8//
+let z = 0;
+if (z === 0) {
+    console.log("Zero");
+} else {
+    console.log("Not Zero");
+}
+
+// 9//
+let marks = 32;
+if (marks >= 35) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
+
+// 10//
+let temp = 25;
+if (temp < 20) {
+    console.log("Cold");
+} else if (temp >= 20 && temp <= 30) {
+    console.log("Warm");
+} else {
+    console.log("Hot");
+}
+
+
+
