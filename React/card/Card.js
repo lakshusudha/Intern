a58f19a36dcef3c6204b3dc18ef33559fb5dcf08
lakshusudha <164891763+lakshusudha@users.noreply.github.com>
2025-11-25 10:42:09 +0530

    Create Card.js

---
+++ React/card/Card.js
@@ -0,0 +1,15 @@
+import React from'react';
+import '../App.css'
+
+const Cards=({src,title,description})=>{
+    return(
+        <div className='card1'>
+            <div className='imgg'>
+                <img src={src} alt={title}/>
+            </div>
+            <h1>{title}</h1> 
+            <p>{description}</p>
+            </div>
+    );
+};
+export default Cards
